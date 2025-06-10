ad6397dca26cc4cde9612315c129cc822a2b9873
KissGe83 <kissge83@gmail.com>
2025-06-10 16:09:34 +0200

    Change minor things

--- scripts/k8s-flask-redis-start.sh
+++ scripts/k8s-flask-redis-start.sh
@@ -1,12 +1,12 @@
 !/bin/bash
-microk8s kubectl apply -f ./k8s/config.yaml
-microk8s kubectl apply -f ./k8s/secret.yaml
-microk8s kubectl apply -f ./k8s/hpa.yaml
-microk8s kubectl apply -f ./k8s/redis-pv.yaml
-microk8s kubectl apply -f ./k8s/redis-pvc.yaml
-microk8s kubectl apply -f ./k8s/redis-deployment.yaml
-microk8s kubectl apply -f ./k8s/redis-service.yaml
-microk8s kubectl apply -f ./k8s/flask-deployment.yaml
-microk8s kubectl apply -f ./k8s/flask-service.yaml
-microk8s kubectl apply -f ./k8s/flask-ingress.yaml
+microk8s kubectl apply -f ../k8s/config.yaml
+microk8s kubectl apply -f ../k8s/secret.yaml
+microk8s kubectl apply -f ../k8s/hpa.yaml
+microk8s kubectl apply -f ../k8s/redis-pv.yaml
+microk8s kubectl apply -f ../k8s/redis-pvc.yaml
+microk8s kubectl apply -f ../k8s/redis-deployment.yaml
+microk8s kubectl apply -f ../k8s/redis-service.yaml
+microk8s kubectl apply -f ../k8s/flask-deployment.yaml
+microk8s kubectl apply -f ../k8s/flask-service.yaml
+microk8s kubectl apply -f ../k8s/flask-ingress.yaml
 
--- scripts/k8s-flask-redis-stop.sh
+++ scripts/k8s-flask-redis-stop.sh
@@ -1,11 +1,11 @@
 !/bin/bash
-microk8s kubectl delete -f ./k8s/config.yaml
-microk8s kubectl delete -f ./k8s/secret.yaml
-microk8s kubectl delete -f ./k8s/hpa.yaml
-microk8s kubectl delete -f ./k8s/redis-deployment.yaml
-microk8s kubectl delete -f ./k8s/redis-service.yaml
-microk8s kubectl delete -f ./k8s/flask-deployment.yaml
-microk8s kubectl delete -f ./k8s/flask-service.yaml
-microk8s kubectl delete -f ./k8s/flask-ingress.yaml
-microk8s kubectl delete -f ./k8s/redis-pvc.yaml
-microk8s kubectl delete -f ./k8s/redis-pv.yaml
+microk8s kubectl delete -f ../k8s/config.yaml
+microk8s kubectl delete -f ../k8s/secret.yaml
+microk8s kubectl delete -f ../k8s/hpa.yaml
+microk8s kubectl delete -f ../k8s/redis-deployment.yaml
+microk8s kubectl delete -f ../k8s/redis-service.yaml
+microk8s kubectl delete -f ../k8s/flask-deployment.yaml
+microk8s kubectl delete -f ../k8s/flask-service.yaml
+microk8s kubectl delete -f ../k8s/flask-ingress.yaml
+microk8s kubectl delete -f ../k8s/redis-pvc.yaml
+microk8s kubectl delete -f ../k8s/redis-pv.yaml
